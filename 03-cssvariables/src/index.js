17521ce2d23f8a648594345d8fe3193bb153c294
OliverHan <ohan88@gmail.comgit config --global user.email ohan88@gmail.com>
2018-08-07 11:19:20 -0700

    finished vanilla 03 css styler

--- 03-cssvariables/src/index.js
+++ 03-cssvariables/src/index.js
@@ -8,9 +8,18 @@ require('./css/index.css');
 type Props = {||};
 
 class Index extends React.Component<Props> {
+	constructor(props) {
+		super(props)
+		this.state = {
+
+		};
+	}
+
 	render() {
 		return (
-			<div className="main">Index</div>
+			<div>
+				<h2>Update CSS Variables with <span class='hl'>JS</span></h2>
+			</div>
 		)
 	}
 }
